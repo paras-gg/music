51d20fbd1c61c71228a6c2782ddcbe1ae0afcac9
paras-gg <adipatifarras@gmail.com>
2025-05-18 15:43:46 +0700

    music

---
+++ pagecari.js
@@ -0,0 +1,6 @@
+if(!window.matchMedia("(hover:hover)").matches){
+    document.getElementsByClassName("gambartombolcari")[0].addEventListener('click', () =>{
+        window.open("pagecari.html", "_blank")
+    })
+}
+print("Halo, dunia!")
